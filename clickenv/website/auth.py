8e97d83f4bbb027385db0748711c3b54a5f9a4cd
Jibril Isah <jibrilisah1@gmail.com>
2023-06-09 05:46:15 +0100

    commit 4

--- clickenv/website/auth.py
+++ clickenv/website/auth.py
@@ -1,16 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, url_for
  
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/login/forget-password', methods=['GET', 'POST'])
+def recover1():
+    return render_template("forget-password.html")
+
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return "<p>Signup</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Enter a valid email', category='error')
+        elif len(firstName) < 2 :
+            flash('First name must be greater than 1 character', category='error')
+        elif len(lastName) < 2 :
+            flash('Last name must be greater than 1 character', category='error')
+        elif len(password1) < 7 :
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("signup.html")
--- clickenv/website/views.py
+++ clickenv/website/views.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def home_page():
+@views.route('/', methods=['GET'])
+def home():
     return render_template("home.html")
+
+@views.route('/blog', methods=['GET'])
+def blog():
+    return render_template("blog.html")
+
+@views.route('/about', methods=['GET'])
+def about():
+    return render_template("about.html")
+
+@views.route('/portfolio', methods=['GET'])
+def portfolio():
+    return render_template("portfolio.html")
+
+@views.route('/dashboard', methods=['GET'])
+def dashboard():
+    return render_template("dashboard.html")
